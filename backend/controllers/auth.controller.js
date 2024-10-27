bb3f481465d517cdcd09812fadca9b2ea32f1e88
Shruti Mehta <shruti.mehta@nagarro.com>
2024-10-27 08:16:21 +0530

    1-project setup

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,17 @@
+export const signup = async (req, res) => {
+  res.json({
+    data: "you hit the signup",
+  });
+};
+
+export const login = async (req, res) => {
+  res.json({
+    data: "you hit the login",
+  });
+};
+
+export const logout = async (req, res) => {
+  res.json({
+    data: "you hit the logout",
+  });
+};
